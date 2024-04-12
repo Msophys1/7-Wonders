31b87c17cbe5c327500d50b3e38cf1a27f74ae1c
zelhajjdib <zelhajjdib@gmail.com>
2024-04-12 15:53:04 +0200

    carte

--- carte.cpp
+++ carte.cpp
@@ -3,32 +3,13 @@
 #include "carte.h"
 using namespace std;
 
-string Carte::ageToString(Age age) {
-    switch(age) {
-        case AGE_I: return "I";
-        case AGE_II: return "II";
-        case AGE_III: return "III";
-        default: return "Invalid Age";
-    }
-}
 
-string Carte::RessourceToString(Ressource ressource) {
-    switch(ressource) {
-        case Bois: return "Bois";
-        case Pierre: return "Pierre";
-        case Verre: return "Verre";
-        case Papyrus: return "Papyrus";
-        case Argile: return "Argile";
-        case Aucune: return "Aucune";
-        default: return "Ressource invalide";
-    }
-}
 
 string const Carte::getNom() {
     return nom;
 }
 
-int const Carte::getPrix(){
+Prix const Carte::getPrix(){
     return prix;
 }
 
@@ -36,13 +17,8 @@ string const Carte::getAge() {
     return ageToString(age);
 }
 
-string const Carte::getRessource() {
-    return RessourceToString(effet.ressource);
-
-}
-
-int const Carte::getRecompense(){
-    return effet.recompense;
+Effet const Carte::getEffet(){
+    return effet;
 }
 
 string const Carte::getCondition(){
--- carte.h
+++ carte.h
@@ -9,35 +9,82 @@ enum Age {AGE_I , AGE_II, AGE_III};
 
 enum Ressource {Argile, Bois, Pierre, Verre, Papyrus, Aucune};
 
+enum Batiment {
+    Civil,
+    Commercial,
+    Scientifique,
+    Militaire,
+    Guildes,
+    Merveilles,
+    Conflit
+};
+
+enum SymboleScientifique {
+    Roue,
+    Balance,
+    Tablet,
+    Pierre,
+    Ampoule,
+    Loupe,
+    Compas
+};
+
+enum Construction {
+    grise,
+    marron,
+    jaune,
+    rouge,
+    merveille
+};
+
 typedef struct Effet{
     Ressource ressource;
+    Ressource ressource1;
+    Ressource ressource2;
+    Ressource ressource3;
+    Ressource ressource4;
+    int pointsDeVictoire;
+    int nbDeBouclier;
     int recompense;
+    string condition;
+    SymboleScientifique symbole;
+    Construction construct;
+    Construction construct1;
+    bool EntreLesDeuxCites;
 }Effet;
+
+typedef struct Prix{
+    Ressource ressource;
+    Ressource ressource1;
+    Ressource ressource2;
+    Ressource ressource3;
+    Ressource ressource4;
+    int cost;
+    string condition;
+}Prix;
 
 class Carte{
     private:
         string nom;
         Effet effet;
         Age age;
-        int prix;
-        string condition;
+        Prix prix;
 
     public:
-        Carte (string n, Age g, int p, Effet e, string c): nom(n), age(g), prix(p), effet(e), condition(c) {};
+        Carte (string n, Age g, Prix p, Effet e): nom(n), age(g), prix(p), effet(e) {};
         Carte(const Carte& c) = delete;
         Carte& operator=(const Carte& c) = delete ;
 
 
 
-        string ageToString(Age age);
-        string RessourceToString(Ressource ressource);
 
-        string const getNom() ;
-        int const getPrix();
-        string const getAge() ;
-        string const getRessource() ;
-        int const getRecompense();
-        string const getCondition();
+        string const getNom();
+        Prix const getPrix();
+        Age const getAge();
+        Effet const getEffet();
+
+
+
 
 
 };
