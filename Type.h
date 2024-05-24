482a1ee6e370bb82d189358e3bd18e58fd5f0c1b
dinamouayed <140742993+dinamouayed@users.noreply.github.com>
2024-05-24 19:20:44 +0200

    Create Type.h

---
+++ Type.h
@@ -0,0 +1,24 @@
+enum Ressources { bois, argile, pierre };
+
+enum class JetonsProgres{
+    Agriculture, Architecture, Economie, Loi, Maconnerie, Mathematiques,
+    Philosophie, Strategie, Theologie, Urbanisme
+};
+
+enum class SymboleScientifiques{
+    Roue, Compas, Mortier_Pilon, Tablette, Lyre, Mesure, Telescope
+};
+enum class produitManufacture{
+    Verre, Papyrus
+};
+
+enum class Types{
+    Scientifique, Commerce, Militaire, Civil, Premiere, Manufacture
+};
+enum class Couleurs{ marron, gris, bleu, vert, jaune, rouge
+};
+
+enum class Chainage{
+    Livre, Engrenage, Masque, Lune, Eau, Sabot, Epee, Tour, Cible, Chevalier, Lyre, Theiere,
+    Soleil, Colonne, Tambour, Vase, Batiment
+};
