7bc43a71282b558462fa30d8e045262e1dd50849
dinamouayed <140742993+dinamouayed@users.noreply.github.com>
2024-05-24 20:22:07 +0200

    Nouveau Joueur.cpp

--- Joueur.cpp
+++ Joueur.cpp
@@ -1,34 +1,9 @@
 #include "Joueur.h"
 
-// Ajouter une ressource au joueur
-void Joueur::ajouterRessource(const Ressources& ressource) {
-    if (nombreRessources < 100) {
-        ressourcesObtenus[nombreRessources] = ressource;
-        nombreRessources++;
-    } else {
-        // Gérer le cas où le tableau est plein
-        throw "Le joueur a atteint la limite de ressources.";
+ostream& operator<<(ostream& f, const Joueur& joueur) {
+    f << "Cartes du joueur " << joueur.getNom() << ":" << std::endl;
+    for (const Carte& carte : joueur.getCartes()) {
+        f << carte << endl;
     }
-}
-
-// Ajouter un symbole scientifique au joueur
-void Joueur::ajouterSymboleScientifique(const SymboleScientifiques& symbole) {
-    if (nombreSymbolesScientifiques < 7) {
-        symbolesScientifiquesObtenus[nombreSymbolesScientifiques] = symbole;
-        nombreSymbolesScientifiques++;
-    } else {
-        // Gérer le cas où le tableau est plein
-        throw "Le joueur a atteint la limite de symboles scientifiques.";
-    }
-}
-
-// Ajouter un jeton de progrès au joueur
-void Joueur::ajouterJetonsProgres(const JetonsProgres& jeton) {
-    if (nombres_jetons_progres < 10) {
-        jetonsPogresObtenus[nombres_jetons_progres] = jeton;
-        nombres_jetons_progres++;
-    } else {
-        // Gérer le cas où le tableau est plein
-        throw "Le joueur a atteint la limite de jetons de progrès.";
-    }
+    return f;
 }
