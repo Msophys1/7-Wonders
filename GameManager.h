42d8e886a330188e91eea8b974c4dd3414c7ba91
dinamouayed <140742993+dinamouayed@users.noreply.github.com>
2024-05-25 11:42:38 +0200

    victoirescientifique GameManager.h

--- GameManager.h
+++ GameManager.h
@@ -1,72 +1,197 @@
-#ifndef GAMEMANAGER_H
-#define GAMEMANAGER_H
+#include "GameManager.h"
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <random>
-#include <ctime>
+void GameManager::initialiserJeu() {
+    selectionnerJetonsProgres();
+    choisirMerveilles();
+}
 
-#include "Type.h"
-#include "Joueur.h"
-#include "Plateau.h"
-#include "Carte.h"
+void GameManager::selectionnerJetonsProgres() {
+    plateau.preparerJetonsDeProgres();
+}
 
-using namespace std;
+void GameManager::choisirMerveilles() {
+    // Mélanger et choisir 4 merveilles aléatoires
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(merveilles.begin(), merveilles.end(), g);
 
-class GameException {
-public:
-    GameException(const std::string& i) :info(i) {}
-    std::string getInfo() const { return info; }
-private:
-    std::string info;
-};
+    vector<Merveille> merveillesChoisies(merveilles.begin(), merveilles.begin() + 4);
+    vector<Merveille> merveillesRestantes(merveilles.begin() + 4, merveilles.end());
 
-class GameManager {
-private:
-    vector<Batiment> batiment; // A creer dans initialisation (static ?)
-    vector<Merveille> merveilles; // A creer dans initialisation (static ?)
-    vector<Guilde> guilde; // A creer dans initialisation (static ?)
+    auto choisirEtAfficherMerveilles = [&](Joueur& joueur1, Joueur& joueur2) {
+        // Afficher 4 merveilles
+        cout << "Merveilles disponibles:" << endl;
+        for (size_t i = 0; i < 4; ++i) {
+            cout << i + 1 << ". " << merveillesChoisies[i].getNom() << endl;
+        }
 
-    Joueur joueur1;
-    Joueur joueur2;
-    Plateau plateau;
-    vector<JetonsProgres> jetonsProgresDisponibles;
-    vector<Carte> defausse;
+        // Joueur 1 choisit une merveille
+        int choix;
+        cout << joueur1.getNom() << ", choisissez une merveille (1-4): ";
+        cin >> choix;
+        while (choix < 1 || choix > 4) {
+            cout << "Choix invalide. Veuillez choisir une merveille (1-4): ";
+            cin >> choix;
+        }
+        joueur1.ajouterMerveille(merveillesChoisies[choix - 1]);
+        merveillesChoisies.erase(merveillesChoisies.begin() + (choix - 1));
 
-public:
-    // Constructeur
-    GameManager(const Joueur& j1, const Joueur& j2) : joueur1(j1), joueur2(j2) {joueur1.setAdversaire(&joueur2), joueur2.setAdversaire(&joueur1);}
+        // Joueur 2 choisit deux merveilles
+        for (int i = 0; i < 2; ++i) {
+            cout << joueur2.getNom() << ", choisissez une merveille (1-" << 4 - i << "): ";
+            cin >> choix;
+            while (choix < 1 || choix > 4 - i) {
+                cout << "Choix invalide. Veuillez choisir une merveille (1-" << 4 - i << "): ";
+                cin >> choix;
+            }
+            joueur2.ajouterMerveille(merveillesChoisies[choix - 1]);
+            merveillesChoisies.erase(merveillesChoisies.begin() + (choix - 1));
+        }
 
-    // Accesseurs
-    Joueur getJoueur1() const { return joueur1; }
-    Joueur getJoueur2() const { return joueur2; }
-    Plateau getPlateau() const { return plateau; }
+        // Attribuer la dernière merveille à joueur1
+        joueur1.ajouterMerveille(merveillesChoisies[0]);
+        merveillesChoisies.clear();
+    };
 
-    const vector<Batiment>& getBatiment() const { return batiment; }
-    const vector<Merveille>& getMerveilles() const { return merveilles; }
-    const vector<Guilde>& getGuilde() const { return guilde; }
-    const vector<JetonsProgres>& getJetonsProgresDisponibles() const { return jetonsProgresDisponibles; }
-    const vector<Carte>& getDefausse() const { return defausse; }
+    // Premier tour de choix de merveilles
+    choisirEtAfficherMerveilles(joueur1, joueur2);
 
-    // Setteurs
-    void ajouterBatiment(const Batiment& b) { batiment.push_back(b); }
-    void ajouterMerveille(const Merveille& m) { merveilles.push_back(m); }
-    void ajouterGuilde(const Guilde& g) { guilde.push_back(g); }
-    void ajouterJetonProgres(const JetonsProgres& jp) { jetonsProgresDisponibles.push_back(jp); }
-    void ajouterCarteDefausse(const Carte& c) { defausse.push_back(c); }
+    // Deuxième tour de choix de merveilles
+    merveillesChoisies.insert(merveillesChoisies.end(), merveillesRestantes.begin(), merveillesRestantes.begin() + 4);
+    choisirEtAfficherMerveilles(joueur2, joueur1);
+}
 
-    // Méthodes de gestion du jeu
-    void initialiserJeu();
-    void selectionnerJetonsProgres();
-    void choisirMerveilles();
-    void commencerPartie();
-    void victoireCivile();
-    int compterDoublonsSymbolesScientifiques(const Joueur& joueur) const;
+int GameManager::compterDoublonsSymbolesScientifiques(const Joueur& joueur) const {
+    map<SymboleScientifiques, int> occurrences;
+    int doublons = 0;
 
-    // Destructeur
-    ~GameManager() = default;
-};
+    // Itérer à travers les symboles scientifiques du joueur
+    for (const auto& symbole : joueur.getSymbolesScientifiques()) {
+        // Incrémenter le nombre d'occurrences de chaque symbole
+        occurrences[symbole]++;
+    }
 
-#endif // GAMEMANAGER_H
+    // Compter les doublons (deux occurrences du même symbole)
+    for (const auto& pair : occurrences) {
+        if (pair.second > 1) {
+            doublons += pair.second / 2;
+        }
+    }
+    return doublons;
+}
+
+void GameManager::commencerPartie() {
+    bool partiefini = false;
+    bool victoire = false;
+    Joueur* current_player = &joueur1;
+
+    while (!partiefini){
+        // Victoire militaire :
+        if (plateau.checkVictoireMilitaire()) {
+            if(current_player == &joueur1 ) cout << "Fin du jeu. "<< joueur1.getNom() << "a remporter la partie par une victoire militaire !" << endl;
+            else cout << "Fin du jeu. "<< joueur2.getNom() << "a remporter la partie par une victoire militaire !" << endl;
+            partiefini = true;
+            victoire = true;
+        }
+
+        // Victoire scientifique :
+        if(compterDoublonsSymbolesScientifiques(joueur1) >= 7)
+        {
+            cout << "Fin du jeu. "<< joueur1.getNom() << "a remporter la partie par une victoire scientifique !" << endl;
+            partiefini = true;
+            victoire = true;
+        }
+        if(compterDoublonsSymbolesScientifiques(joueur2) >= 7)
+        {
+            cout << "Fin du jeu. "<< joueur2.getNom() << "a remporter la partie par une victoire scientifique !" << endl;
+            partiefini = true;
+            victoire = true;
+        }
+
+
+        // Prochain joueur
+        if(current_player == &joueur1 && joueur1.getEffetRejouer()) current_player = &joueur1;
+        else if(current_player == &joueur2 && joueur2.getEffetRejouer()) current_player = &joueur2;
+        else if(current_player == &joueur1) current_player = &joueur2;
+        else current_player = &joueur1;
+    }
+
+    // Victoire civile
+    if(!victoire)
+    {
+        victoireCivile();
+    }
+}
+
+void GameManager::victoireCivile() {
+    //Decompte des points et attribution de la victoire
+    unsigned int totalPointJoueur1 = 0;
+    unsigned int totalPointJoueur2 = 0;
+
+    // Points de victoire par bâtiment
+    for(size_t i = 0; i < joueur1.getBatiment().size(); ++i) {
+        totalPointJoueur1 += joueur1.getBatiment()[i].getPointVictoire();
+    }
+    for(size_t i = 0; i < joueur2.getBatiment().size(); ++i) {
+        totalPointJoueur2 += joueur2.getBatiment()[i].getPointVictoire();
+    }
+
+    // Points de victoire Merveilles
+    for(size_t i = 0; i < joueur1.getMerveilles().size(); ++i) {
+        totalPointJoueur1 += joueur1.getMerveilles()[i].getPointVictoire();
+    }
+    for(size_t i = 0; i < joueur2.getMerveilles().size(); ++i) {
+        totalPointJoueur2 += joueur2.getMerveilles()[i].getPointVictoire();
+    }
+
+    // Points de victoire Progres
+    for(size_t i = 0; i < joueur1.getJetonsProgres().size(); ++i) {
+        if(joueur1.getJetonsProgres()[i] == JetonsProgres::Agriculture) {
+            totalPointJoueur1 += 4;
+        }
+        if(joueur1.getJetonsProgres()[i] == JetonsProgres::Philosophie) {
+            totalPointJoueur1 += 7;
+        }
+    }
+    for(size_t i = 0; i < joueur2.getJetonsProgres().size(); ++i) {
+        if(joueur2.getJetonsProgres()[i] == JetonsProgres::Agriculture) {
+            totalPointJoueur2 += 4;
+        }
+        if(joueur2.getJetonsProgres()[i] == JetonsProgres::Philosophie) {
+            totalPointJoueur2 += 7;
+        }
+    }
+
+    // TODO : Points de victoire militaire
+
+    // TODO : Points de victoire guildes
+
+
+    // Trésor de la cité
+    totalPointJoueur1 += joueur1.getPieces() / 3;
+    totalPointJoueur2 += joueur2.getPieces() / 3;
+
+    if(totalPointJoueur1 == totalPointJoueur2)
+    {
+        cout << "Fin de la partie. Égalité des points. La partie se départagera en fonction des bâtiments civils." << endl;
+        unsigned int totalPointCarteBleu1 = 0;
+        unsigned int totalPointCarteBleu2 = 0;
+        for(size_t i = 0; i < joueur1.getBatiment().size(); ++i) {
+            if(joueur1.getBatiment()[i].getType() == type_batiment::Civil) {
+                totalPointCarteBleu1 += joueur1.getBatiment()[i].getPointVictoire();
+            }
+        }
+        for(size_t i = 0; i < joueur2.getBatiment().size(); ++i) {
+            if(joueur2.getBatiment()[i].getType() == type_batiment::Civil) {
+                totalPointCarteBleu1 += joueur2.getBatiment()[i].getPointVictoire();
+            }
+        }
+        if(totalPointCarteBleu1 > totalPointCarteBleu2) cout << "Fin du jeu. "<< joueur1.getNom() << "a remporter la partie !" << endl;
+        else cout << "Fin du jeu. "<< joueur2.getNom() << "a remporter la partie par une victoire Civile!" << endl;
+    }
+    else {
+        if(totalPointJoueur1 > totalPointJoueur2) cout << "Fin du jeu. "<< joueur1.getNom() << "a remporter la partie !" << endl;
+        else cout << "Fin du jeu. "<< joueur2.getNom() << "a remporter la partie par une victoire Civile !" << endl;
+    }
+}
+
