54a245325dbf4cee535409bc794526cc84742164
dinamouayed <140742993+dinamouayed@users.noreply.github.com>
2024-05-25 16:24:36 +0200

    Update Carte.cpp Type mis a jour

--- Carte.cpp
+++ Carte.cpp
@@ -1,122 +1,124 @@
-#include "wonders.h"
+#include "Carte.h"
+#include "GameManager.h"
 
-std::set<ressource> getMatierePremiere() {
+std::set<Ressources> getMatierePremiere() {
     return {
-        ressource::Pierre,
-        ressource::Argile,
-        ressource::Bois
+            Ressources::Pierre,
+            Ressources::Argile,
+            Ressources::Bois
     };
 }
 
-std::set<ressource> getProduitManufacture() {
+std::set<Ressources> getProduitManufacture() {
     return {
-        ressource::Papyrus,
-        ressource::Verre
+            Ressources::Papyrus,
+            Ressources::Verre
     };
 }
 
-std::set<ressource> getRessource() {
+std::set<Ressources> getRessource() {
     return {
-        ressource::Pierre,
-        ressource::Argile,
-        ressource::Bois,
-        ressource::Papyrus,
-        ressource::Verre
+            Ressources::Pierre,
+            Ressources::Argile,
+            Ressources::Bois,
+            Ressources::Papyrus,
+            Ressources::Verre
     };
 }
 
-std::set<ressource> getSymboleScientifique() {
+std::set<Ressources> getSymboleScientifique() {
     return {
-        ressource::Roue,
-        ressource::Compas, 
-        ressource::Pilon, 
-        ressource::Tablette, 
-        ressource::Lyre, 
-        ressource::Mesure, 
-        ressource::Telescope
+            Ressources::Roue,
+            Ressources::Compas,
+            Ressources::Pilon,
+            Ressources::Tablette,
+            Ressources::Lyre,
+            Ressources::Mesure,
+            Ressources::Telescope
     };
 }
 
-std::string tostringRessources(ressource r){
+std::string tostringRessources(Ressources r){
     switch(r) {
-        case ressource::Roue : return "Roue" ; 
-        case ressource::Compas : return "Compas" ; 
-        case ressource::Pilon : return "Pilon" ; 
-        case ressource::Tablette : return "Tablette" ; 
-        case ressource::Lyre : return "Lyre" ; 
-        case ressource::Mesure : return "Mesure" ; 
-        case ressource::Telescope : return "Telescope" ; 
-        case ressource::Bouclier : return "Bouclier" ; 
-        case ressource::Papyrus : return "Papyrus" ; 
-        case ressource::Verre : return "Verre" ; 
-        case ressource::Pierre : return "Pierre" ; 
-        case ressource::Argile : return "Argile" ; 
-        case ressource::Bois : return "Bois" ; 
+        case Ressources::Roue : return "Roue" ;
+        case Ressources::Compas : return "Compas" ;
+        case Ressources::Pilon : return "Pilon" ;
+        case Ressources::Tablette : return "Tablette" ;
+        case Ressources::Lyre : return "Lyre" ;
+        case Ressources::Mesure : return "Mesure" ;
+        case Ressources::Telescope : return "Telescope" ;
+        case Ressources::Bouclier : return "Bouclier" ;
+        case Ressources::Papyrus : return "Papyrus" ;
+        case Ressources::Verre : return "Verre" ;
+        case Ressources::Pierre : return "Pierre" ;
+        case Ressources::Argile : return "Argile" ;
+        case Ressources::Bois : return "Bois" ;
         default : throw GameException("Ressource inconnue") ;
     };
 }
 
-std::string tostringType(type_batiment t){
+std::string tostringType(Types t){
     switch(t){
-        case type_batiment::Civil : return "Civil" ; 
-        case type_batiment::Militaire : return "Civil" ; 
-        case type_batiment::Scientifique : return "Civil" ;
-        case type_batiment::Manufacture : return "Civil" ;
-        case type_batiment::Premiere : return "Civil" ;
-        case type_batiment::Commerce : return "Civil" ;
-        case type_batiment::Guilde : return "Civil" ;
-        case type_batiment::Merveille : return "Civil" ;
+        case Types::Civil : return "Civil" ;
+        case Types::Militaire : return "Civil" ;
+        case Types::Scientifique : return "Civil" ;
+        case Types::Manufacture : return "Civil" ;
+        case Types::Premiere : return "Civil" ;
+        case Types::Commerce : return "Civil" ;
+        case Types::Guilde : return "Civil" ;
+        case Types::Merveille : return "Civil" ;
         default : throw GameException("Batiment inconnue") ;
     };
 }
 
 
-bool Carte::checkCoutRessources(std::list<ressource> cost_r) const {
-    std::set<ressource> standard_ressources = getRessource(); 
-    for(auto iter = cost_r.begin() ; iter != cost_r.end() ; ++iter ) { // list iterator 
+bool Carte::checkCoutRessources(std::list<Ressources> cost_r) const {
+    std::set<Ressources> standard_ressources = getRessource();
+    for(auto iter = cost_r.begin() ; iter != cost_r.end() ; ++iter ) { // list iterator
         if( standard_ressources.find(*iter) == standard_ressources.end() ) {
             //std::cout << "WARNING : INVALID RESSOURCE LIST" << std::endl ;
-            return false ; 
+            return false ;
         }
     }
     //std::cout << "CORRECT RESSOURCE LIST" << std::endl ;
-    return true ; 
-} 
-
-Carte::Carte(std::string n, type_batiment t, std::list<ressource> c_r, unsigned int c_m, unsigned int arge, unsigned int vic){
-    nom = n ; 
-    type = t ; 
-
-    if(c_r.size() > 5){
-        throw GameException("ERREUR : trop de ressources dans le coût de la carte instanciée") ; 
-    }
-
-    cost_m = c_m ; 
-    
-    if(!checkCoutRessources(c_r)){
-        throw GameException("ERREUR : list de ressources invalide dans la carte instanciée (matières premières ou produits manufacturés SEULEMENT)") ;
-    } 
-    cost_r = c_r ;
-
-    argent = arge ; 
-    pt_victoire = vic ; 
+    return true ;
 }
 
-void Carte::setCoutRessource(std::list<ressource> cost){
+Carte::Carte(std::string n, Types t, std::list<Ressources> c_r, unsigned int c_m, unsigned int arge, unsigned int vic){
+    nom = n ;
+    type = t ;
+
+    if(c_r.size() > 5){
+        throw GameException("ERREUR : trop de ressources dans le coût de la carte instanciée") ;
+    }
+
+    cost_m = c_m ;
+
+    if(!checkCoutRessources(c_r)){
+        throw GameException("ERREUR : list de ressources invalide dans la carte instanciée (matières premières ou produits manufacturés SEULEMENT)") ;
+    }
+    cost_r = c_r ;
+
+    argent = arge ;
+    pt_victoire = vic ;
+}
+
+
+void Carte::setCoutRessource(std::list<Ressources> cost){
     if(checkCoutRessources(cost) && cost.size() <= 5) {
-        cost_r = cost ; 
+        cost_r = cost ;
     } else {
         throw GameException("ERREUR : list de ressources invalide dans la carte à modifier (matières premières ou produits manufacturés SEULEMENT)") ;
     }
-}    
+}
 
 std::ostream& operator<<(std::ostream& f, Carte c){
-    f << c.getNom() << std::endl ; 
-    f << "COUT ARGENT: " << c.getCoutArgent() << std::endl ; 
+    f << c.getNom() << std::endl ;
+    f << "COUT ARGENT: " << c.getCoutArgent() << std::endl ;
     f << "COUT RESSOURCES: " ;
-    std::list<ressource> res = c.getCoutRessource();
+    std::list<Ressources> res = c.getCoutRessource();
     for(auto iter = res.begin() ; iter != res.end() ; ++iter) { f << tostringRessources(*iter) << " ; " ; }
     f << std::endl ;
     f << "REWARDS: " << c.getRewardArgent() << " pièces ; " << c.getPointVictoire() << " points victoire" << std::endl ;
-    return f ; 
+    return f ;
 }
