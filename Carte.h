d7abe4f94fa3caae40cbcdb0b858ba71e749d060
dinamouayed <140742993+dinamouayed@users.noreply.github.com>
2024-05-26 19:30:42 +0200

    Update Carte.h debug

--- Carte.h
+++ Carte.h
@@ -4,18 +4,15 @@
 #include <string>
 #include <list>
 #include <set>
+
 #include "Type.h"
 
-
-//enum class Ressources {Papyrus, Verre, Pierre, Argile, Bois};
-//enum class symbole_scientifique {Roue, Compas, Pilon, Tablette, Lyre, Mesure, Telescope};
-//enum class Types {Militaire, Scientifique, Manufacture, Premiere, Civil, Commerce, Guilde, Merveille};
-//enum class jeton_progres{Agriculture, Architecture, Economie, Loi, Maconnerie, Mathematiques,Philosophie, Strategie, Theologie, Urbanisme};
-//enum class phase_jeu {START, AGE_I, AGE_II, AGE_III, END};
-
-//enum class Ressources {Bouclier, Papyrus, Verre, Pierre, Argile, Bois, Roue, Compas, Pilon, Tablette, Lyre, Mesure, Telescope};
-// toutes les Ressourcess génériques sont centralisées dans cet enum : 
-// Boucliers, Matières Premières, Produits Manufacturés, Symbols Scientifiques
+#ifndef CARTE_H
+#define CARTE_H
+//enum class type_batiment { Militaire, Scientifique, Manufacture, Premiere, Civil, Commerce, Guilde, Merveille };
+//enum class JetonsProgres { Agriculture, Architecture, Economie, Loi, Maconnerie, Mathematiques, Philosophie, Strategie, Theologie, Urbanisme };
+//enum class phase_jeu { START, AGE_I, AGE_II, AGE_III, END };
+//enum class ressource { Bouclier, Papyrus, Verre, Pierre, Argile, Bois, Roue, Compas, Pilon, Tablette, Lyre, Mesure, Telescope };
 
 std::set<Ressources> getMatierePremiere();
 std::set<Ressources> getProduitManufacture();
@@ -26,98 +23,96 @@ std::string tostringRessources(Ressources r);
 std::string tostringType(Types t);
 
 class Carte {
-    public: 
+public:
 
-        Carte(std::string nom, Types type, std::list<Ressources> cost_r = {}, unsigned int cost_m = 0, unsigned int argent = 0, unsigned int pt_victoire = 0);
+    Carte(std::string nom, Types type, std::list<Ressources> cost_r = {}, unsigned int cost_m = 0, unsigned int argent = 0, unsigned int pt_victoire = 0);
 
-        // GETTERS
-        std::string getNom() const { return nom ; }
-        Types getType() const { return type ;}
-        unsigned int getCoutArgent() const { return cost_m ; }
-        unsigned int getRewardArgent() const { return argent ; }
-        unsigned int getPointVictoire() const { return pt_victoire ;}
-        std::list<Ressources> getCoutRessource() const { return cost_r ;}
+    // GETTERS
+    std::string getNom() const { return nom ; }
+    Types getType() const { return type ;}
+    unsigned int getCoutArgent() const { return cost_m ; }
+    unsigned int getRewardArgent() const { return argent ; }
+    unsigned int getPointVictoire() const { return pt_victoire ;}
+    std::list<Ressources> getCoutRessource() const { return cost_r ;}
 
-        // SETTERS
-        void setNom(std::string new_nom){ nom = new_nom ;}
-        //void setType(Types new_type) {type = new_type ;} //  attribut const
-        void setCoutArgent(unsigned int new_cost_m) { cost_m = new_cost_m ;}
-        void setRewardArgent(unsigned int new_argent) { argent = new_argent ;}
-        void setPointVictoire(unsigned int new_pt_victoire) { pt_victoire = new_pt_victoire ;}
-        void setCoutRessource(std::list<Ressources> cost) ;
+    // SETTERS
+    void setNom(std::string new_nom){ nom = new_nom ;}
+    //void setType(Types new_type) {type = new_type ;} //  attribut const
+    void setCoutArgent(unsigned int new_cost_m) { cost_m = new_cost_m ;}
+    void setRewardArgent(unsigned int new_argent) { argent = new_argent ;}
+    void setPointVictoire(unsigned int new_pt_victoire) { pt_victoire = new_pt_victoire ;}
+    void setCoutRessource(std::list<Ressources> cost) ;
 
-        // UTILS
-        bool achetableRessource(std::list<Ressources> buy) const ; 
-    
-    protected : 
+    // UTILS
+    bool achetableRessource(std::list<Ressources> buy) const ;
 
-        bool checkTypeRessources(std::list<Ressources> cost_r, std::set<Ressources> subset) const;
-        // vérifier que les Ressourcess entrées appartiennent à la bonne catégorie
+protected :
 
-    private:
-        std::string nom ;
-        const Types type ;
-        std::list<Ressources> cost_r ; // cout en Ressourcess
-        unsigned int cost_m ; // cout en argent
+    bool checkTypeRessources(std::list<Ressources> cost_r, std::set<Ressources> subset) const;
+    // vérifier que les Ressourcess entrées appartiennent à la bonne catégorie
 
-        unsigned int argent ;
-        unsigned int pt_victoire ;
+private:
+    std::string nom ;
+    const Types type ;
+    std::list<Ressources> cost_r ; // cout en Ressourcess
+    unsigned int cost_m ; // cout en argent
+
+    unsigned int argent ;
+    unsigned int pt_victoire ;
 };
+
 
 std::ostream& operator<<(std::ostream& f, Carte c);
 
-class Batiment : public Carte { 
+class Batiment : public Carte {
     // Civil, Scientifique, Militaire, Ressources
-    public:
+public:
 
-        Batiment(
-            std::string nom, Types type, 
-            std::list<Ressources> cost_r={}, unsigned int cost=0, 
+    Batiment(
+            std::string nom, Types type,
+            std::list<Ressources> cost_r={}, unsigned int cost=0,
             unsigned int argent=0, unsigned int pt_victoire=0,
 
             std::list<Ressources> production={}, std::string chained_by=""
-            );
-        
-        // GETTERS
-        std::string getChainage() const { return chained_by;}
-        std::list<Ressources> getProduction() const { return production ;}
+    );
 
-        // SETTERS
-        void setChainage(std::string new_chain) { chained_by = new_chain;}
-        void setProduction(std::list<Ressources> new_prod) { production = new_prod ;}
+    // GETTERS
+    std::string getChainage() const { return chained_by;}
+    std::list<Ressources> getProduction() const { return production ;}
+
+    // SETTERS
+    void setChainage(std::string new_chain) { chained_by = new_chain;}
+    void setProduction(std::list<Ressources> new_prod) { production = new_prod ;}
 
 
-    private:
-        std::list<Ressources> production ;
-        std::string chained_by ; 
+private:
+    std::list<Ressources> production ;
+    std::string chained_by ;
 };
 
 class Guilde : public Carte {
-    public:
-    private:
-        Types affectation ; 
-        bool usurier ; 
+public:
+private:
+    Types affectation ;
+    bool usurier ;
 };
 
 class Commerce : public Batiment {
-    public:
-    private:
+public:
+private:
 };
 
 class Merveille : public Carte {
-    public:
-        Merveille(std::string nom, Types type=Types::Merveille, std::list<Ressources> cost_r = {}, unsigned int cost_m = 0, unsigned int argent = 0, unsigned int pt_victoire = 0, bool b=false) : Carte(nom, type, cost_r, cost_m, argent, pt_victoire), replay(b) {
-            if(type!=Types::Merveille){
-                throw GameException("Merveille instanciée avec un type autre que Merveille");
-            }
-        }; 
+public:
+    Merveille(std::string nom, Types type=Types::Merveille, std::list<Ressources> cost_r = {}, unsigned int cost_m = 0, unsigned int argent = 0, unsigned int pt_victoire = 0, bool b=false);
 
-        //GETTERS
-        bool getReplay() const { return replay ;}
+    //GETTERS
+    bool getReplay() const { return replay ;}
 
-        //SETTERS
-        void setReplay(bool b) { replay = b ;}
+    //SETTERS
+    void setReplay(bool b) { replay = b ;}
 
-    private:
-        bool replay ; 
+private:
+    bool replay ;
 };
+#endif // CARTE_H
