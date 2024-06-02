b4bb15695b5446e0f4dcd8a41ad6b52f2f16cfd4
Nils <69086570+jin-sei@users.noreply.github.com>
2024-06-02 17:45:24 +0200

    Update Carte.cpp - guildes et commerces

--- Carte.cpp
+++ Carte.cpp
@@ -72,106 +72,229 @@ std::string tostringType(Types t){
 }
 
 
-bool Carte::checkTypeRessources(std::list<Ressources> cost_r, std::set<Ressources> subset) const {
-    //std::set<Ressources> standard_Ressourcess = getRessource();
-    for(auto iter = cost_r.begin() ; iter != cost_r.end() ; ++iter ) { // list iterator
+
+bool Carte::checkTypeRessourcess(std::list<Ressources> cost_r, std::set<Ressources> subset) const {
+    //std::set<Ressources> standard_Ressourcess = getRessources(); 
+    for(auto iter = cost_r.begin() ; iter != cost_r.end() ; ++iter ) { // list iterator 
         if( subset.find(*iter) == subset.end() ) {
-            //std::cout << "WARNING : INVALID RESSOURCE LIST" << std::endl ;
-            return false ;
+            //std::cout << "WARNING : INVALID Ressources LIST" << std::endl ;
+            return false ; 
         }
     }
-    //std::cout << "CORRECT RESSOURCE LIST" << std::endl ;
-    return true ;
-}
+    //std::cout << "CORRECT Ressources LIST" << std::endl ;
+    return true ; 
+} 
 
-Carte::Carte(std::string n, Types t, std::list<Ressources> c_r, unsigned int c_m, unsigned int arge, unsigned int vic):nom(n), type(t), argent(arge), pt_victoire(vic){
+Carte::Carte(std::string n, Types t,  phase_jeu a, std::list<Ressources> c_r, unsigned int c_m, unsigned int arge, unsigned int vic):nom(n), type(t), c_age(a), argent(arge), pt_victoire(vic){
 
     if(c_r.size() > 5){
-        throw GameException("ERREUR : trop de Ressourcess dans le coût de la carte instanciée") ;
+        throw GameException("ERREUR : trop de Ressourcess dans le coût de la carte instanciée") ; 
     }
 
-    cost_m = c_m ;
-
-    if(!checkTypeRessources(c_r, getRessource())){
+    cost_m = c_m ; 
+    
+    if(!checkTypeRessourcess(c_r, getRessources())){
         throw GameException("ERREUR : list de Ressourcess invalide dans la carte instanciée (matières premières ou produits manufacturés SEULEMENT)") ;
-    }
+    } 
     cost_r = c_r ;
 }
 
-void Carte::setCoutRessource(std::list<Ressources> cost){
-    if(checkTypeRessources(cost, getRessource()) && cost.size() <= 5) {
-        cost_r = cost ;
+void Carte::setCoutRessources(std::list<Ressources> cost){
+    if(checkTypeRessourcess(cost, getRessources()) && cost.size() <= 5) {
+        cost_r = cost ; 
     } else {
         throw GameException("ERREUR : list de Ressourcess invalide dans la carte à modifier (matières premières ou produits manufacturés SEULEMENT)") ;
     }
 }
 
-bool Carte::achetableRessource(std::list<Ressources> buy) const {
-    if(cost_r.empty()) { return true ; }
-    if(cost_r.size() > buy.size()) { return false ; }
-
-    std::list<Ressources> pop_buy = buy ;
-
-    for(auto iter = cost_r.begin() ; iter != cost_r.end() ; ++iter){ // parcours les Ressourcess demandées
-        std::cout << "ASKED: " << tostringRessources(*iter) << std::endl ;
-
-        for(auto iter_b = pop_buy.begin() ; iter_b != pop_buy.end() ; ++iter_b){ // parcours les Ressourcess disponibles
-
-            std::cout << "FOUND: " << tostringRessources(*iter_b) << " ; ";
-
-            if(*iter_b == *iter){ // trouvé la bonne Ressources
-
-                std::cout << " MATCH!" << std::endl;
-                pop_buy.erase(iter_b);
-                break;
-
-            } else if(next(iter_b) == pop_buy.end()){ // on atteint la fin sans trouver de Ressources match : Ressources manquante
-
-                std::cout << "NOT FOUND!" << std::endl;
-                return false;
-            }
-
-        }
-        std::cout << std::endl ;
-    }
-    return true ; // remove
+std::ostream& operator<<(std::ostream& f, const Carte& c){
+    c.affichage();
+    return f ; 
 }
 
-std::ostream& operator<<(std::ostream& f, Carte c){
-    f << c.getNom() << ", " << tostringType(c.getType()) << std::endl ;
-    f << "COUT ARGENT: " << c.getCoutArgent() << std::endl ;
-    f << "COUT RESSOURCES: " ;
-    std::list<Ressources> res(c.getCoutRessource());
-    for(auto iter = res.begin() ; iter != res.end() ; ++iter) { f << tostringRessources(*iter) << " ; " ; }
-    f << std::endl ;
-    f << "REWARDS: " << c.getRewardArgent() << " pièces ; " << c.getPointVictoire() << " points victoire" << std::endl ;
-    return f ;
-}
-
-Merveille::Merveille(std::string nom, Types type, std::list<Ressources> cost_r, unsigned int cost_m, unsigned int argent, unsigned int pt_victoire, bool b) : Carte(nom, type, cost_r, cost_m, argent, pt_victoire), replay(b) {
-    if(type!=Types::Merveille){
-        throw GameException("Merveille instanciée avec un type autre que Merveille");
-    }
-};
-
-Batiment::Batiment(std::string nom, Types type, std::list<Ressources> cost_r, unsigned int cost_m, unsigned int argent, unsigned int pt_victoire, std::list<Ressources> production, std::string chained_by):
-        Carte(nom, type, cost_r, cost_m, argent, pt_victoire), chained_by(chained_by){
-    // vérifications liées au type :
+Batiment::Batiment(std::string nom, Types type, phase_jeu age, std::list<Ressources> cost_r, unsigned int cost_m, unsigned int argent, unsigned int pt_victoire, std::list<Ressources> prod, std::string chained_by):
+Carte(nom, type, age, cost_r, cost_m, argent, pt_victoire), chained_by(chained_by){
+    // vérifications liées au type : 
     if(type == Types::Civil && !production.empty()){
         throw GameException("ERREUR (Batiment instancié) : un bâtiment CIVIL ne produit pas de Ressourcess");
     }
     if(type == Types::Scientifique){
-        if(!checkTypeRessources(production, getSymboleScientifique())){throw GameException("ERREUR (Batiment instancié) : un bâtiment SCIENTIFIQUE ne produit que des symboles scientifiques");}
-        if(production.size() != 1){throw GameException("ERREUR (Batiment instancié) : un bâtiment SCIENTIFIQUE ne produit qu'un seul symbole");}
+        if(!checkTypeRessourcess(prod, getSymboleScientifique())){throw GameException("ERREUR (Batiment instancié) : un bâtiment SCIENTIFIQUE ne produit que des symboles scientifiques");}
+        if(prod.size() != 1){throw GameException("ERREUR (Batiment instancié) : un bâtiment SCIENTIFIQUE ne produit qu'un seul symbole");}
     }
     if(type == Types::Militaire){
-        if(!checkTypeRessources(production, {Ressources::Bouclier})){throw GameException("ERREUR (Batiment instancié) : un bâtiment MILITAIRE ne produit que des boucliers");}
+        if(!checkTypeRessourcess(prod, {Ressources::Bouclier})){throw GameException("ERREUR (Batiment instancié) : un bâtiment MILITAIRE ne produit que des boucliers");}
     }
     if(type == Types::Manufacture){
-        if(!checkTypeRessources(production, getProduitManufacture())){throw GameException("ERREUR (Batiment instancié) : un bâtiment de Ressourcess GRIS ne produit que des produits manufacturés");}
+        if(!checkTypeRessourcess(prod, getProduitManufacture())){throw GameException("ERREUR (Batiment instancié) : un bâtiment de Ressourcess GRIS ne produit que des produits manufacturés");}
     }
     if(type == Types::Premiere){
-        if(!checkTypeRessources(production, getMatierePremiere())){throw GameException("ERREUR (Batiment instancié) : un bâtiment de Ressourcess MARRON ne produit que des matièrs premières");}
+        if(!checkTypeRessourcess(prod, getMatierePremiere())){throw GameException("ERREUR (Batiment instancié) : un bâtiment de Ressourcess MARRON ne produit que des matièrs premières");}
 
     }
+
+    production = prod ; 
+}
+
+void Carte::affichage() const {
+    std::cout << nom << ", " << tostringType(type) << ", " << tostringAge(c_age) << std::endl ; 
+    std::cout << "COUT ARGENT: " << cost_m << std::endl ;
+    std::cout << "COUT RessourcesS: " ;
+    std::list<Ressources> res(getCoutRessources());
+    for(auto iter = res.begin() ; iter != res.end() ; ++iter) { std::cout << tostringRessourcess(*iter) << " ; " ; }
+    std::cout << std::endl ;
+    std::cout << "REWARDS: " << argent << " pièces ; " << pt_victoire << " points victoire" << std::endl ;
+}
+
+void Batiment::affichage() const {
+    Carte::affichage();
+    std::cout << "PRODUCTION: " ;
+    displayRessourcess( production ) ;
+}
+
+void Batiment::onBuild(Joueur* j) const {
+    std::cout << "CALLING ONBUILD BATIMENT" << std::endl ;
+    return ; 
+}
+
+Commerce::Commerce(
+    std::string nom, Types type, phase_jeu age, 
+    std::list<Ressources> cost_r, unsigned int cost, 
+    unsigned int argent, unsigned int pt_victoire, std::list<Ressources> prod, 
+    std::string chained_by, const Perk* perk):
+        Batiment(nom, type,age, cost_r, cost, argent, pt_victoire, prod, chained_by), perk(perk) {
+
+            if( type != Types::Commerce && type != Types::Merveille ){ throw GameException("ERREUR : instanciation d'un bâtiment non Commerce avec la classe Commerce");}
+            // comme le constructeur de Commerce est appelé pour construire Merveille
+            // we need to allow the Commerce constructor to create Merveille types
+
+}
+
+Commerce::~Commerce() { if(perk!=nullptr){delete perk ;} }
+
+void Commerce::onBuild(Joueur* j) const {
+    std::cout << "CALLING ONBUILD COMMERCE" << std::endl ;
+    if(perk!=nullptr){
+        perk->onCall(j); 
+    }
+}
+
+/*
+void Merveille::onBuild(Joueur* j) const {
+
+}
+*/
+
+Guilde::Guilde(
+    std::string nom, Types type, phase_jeu age,
+    std::list<Ressources> cost_r, unsigned int cost,
+    unsigned int argent, unsigned int pt_victoire,
+
+    std::list<Types> affectation, bool usurier):
+    Carte(nom, type, age,cost_r, cost, argent, pt_victoire),affectation(affectation), usurier(usurier){
+        if( type != Types::Guilde ){ throw GameException("ERREUR: Utilisation du constructeur de Guilde avec un Types invalide");}
+}
+
+unsigned int Guilde::ptVictoireFinJeu(Joueur* j) const {
+
+    /*
+    if( usurier ){ return std::max(j->getTresor()/3, j->getAdversaire()->getTresor()/3) ;}
+
+    unsigned int p[2] = {0, 0} ;
+    Joueur* jp[2] = {j, j->getAdversaire()};
+
+    for(size_t i = 0 ; i < 1 ; i++){
+        for( auto iter = affectation.begin() ; iter != affectation.end() ; ++iter ){
+            p[i] += jp[i]->getNumberBatiment(*iter);
+        }
+    }
+
+    return std::max(p[0], p[1]) ;
+    */
+    return 0 ; // TEMP
+
+}
+
+void Guilde::rewardArgent(Joueur* j) const {
+
+    /*
+    unsigned int p[2] = {0, 0} ;
+    Joueur* jp[2] = {j, j->getAdversaire()};
+
+    for(size_t i = 0 ; i < 1 ; i++){
+        for( auto iter = affectation.begin() ; iter != affectation.end() ; ++iter ){
+            p[i] += jp[i]->getNumberBatiment(*iter) * getRewardArgent() ;
+        }
+    }
+
+    j->addTresor( std::max(p[0], p[1]) );
+    */
+}
+
+void Guilde::onBuild(Joueur* j) const {
+    std::cout << "CALLING ONBUILD GUILDE" << std::endl ;
+    //rewardArgent(j);
+}
+
+void Perk_CoinPerCard::gainCoinPerCard(Joueur* j) const {
+    //j->addTresor( j->getNumberBatiment(this->card)*coin );
+}
+
+void Perk_CoinPerCard::onCall(Joueur* j) const {
+    //gainCoinPerCard(j);
+}
+
+void Perk_FixedTrade::setFixedTrade(Joueur* j) const {/*
+    for(auto iter = res.begin() ; iter != res.end() ; ++iter){
+        j->setFixedTrade(*iter, coin);
+    }
+    */
+}
+
+void Perk_FixedTrade::onCall(Joueur* j) const {
+    setFixedTrade(j);
+}
+
+Perk_Classic::Perk_Classic(unsigned int id, const GameManager* box):id(id), box(box){
+    if(id > 2){
+        throw GameException("ERREUR: Classic Perk id is 0 - 1 - 2");
+    }
+    if(box == nullptr){
+        throw GameException("ERREUR: Un pointeur vers Box non nul est requis pour une Perk Classic");
+    }
+}
+
+void Perk_Classic::saccage(Joueur* j) const {
+    //j->getAdversaire()->subTresor(3);
+}
+
+void Perk_Classic::freeConstructionFromDefausse(Joueur* j) const { 
+    // requires player interaction
+}
+
+void Perk_Classic::pickJeton(Joueur* j) const { 
+    // requires player interaction
+}
+
+void Perk_Classic::onCall(Joueur* j) const {
+    switch(id){
+        case 0 : /*saccage(j) ;*/ break ;
+        case 1 : /*pickJeton(j) ;*/ break ; 
+        case 2 : /*freeConstructionFromDefausse(j) ;*/ break ; 
+        default : throw GameException("ERREUR: unknown Classic Perk id") ; 
+    }
+}
+
+Perk_Destruction::Perk_Destruction(type_batiment c):card(c){
+    if(c != type_batiment::Premiere || c != type_batiment::Manufacture){
+        throw GameException("ERREUR: La destruction par Perk Destruction n'est permise que sur Batiment Premiere ou Manufacture");
+    }
+}
+
+void Perk_Destruction::destruction(Joueur* j) const {
+    // requires player interaction
+}
+
+void Perk_Destruction::onCall(Joueur* j) const {
+    // destruction(j);
 }
