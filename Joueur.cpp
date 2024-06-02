ded45e548cd046a7681af1ed138dad20b20c8f3a
dinamouayed <140742993+dinamouayed@users.noreply.github.com>
2024-06-02 17:53:42 +0200

    Update Joueur.cpp construction d'une merveille + construction d'un batiment MAJ

--- Joueur.cpp
+++ Joueur.cpp
@@ -34,6 +34,7 @@ vector<Ressources> Joueur::getAllRessources() const {
 }
 
 void Joueur::construireBatiment(const Batiment& bat){
+    // TODO : Si ajout militaire ? Traitement des batiments militaire.
     unsigned int coutArgent = bat.getCoutArgent();
     if(pieces < coutArgent)
     {
@@ -57,5 +58,39 @@ void Joueur::construireBatiment(const Batiment& bat){
         }
     }
     pieces -= coutArgent;
+    pointsDeVictoire += bat.getPointVictoire();
+
+    if (bat.getType() == Types::Scientifique) {
+        // TODO : ajout du symbole scientifique dans les attribut du joueur
+    }
+
     ajouterBatiment(bat);
+}
+
+void Joueur::construireMerveille(const Merveille &merveille) {
+    // N.B : Une merveille ne s'achete qu'en ressource, donc pas besoin de verifier les pieces du joueur
+
+    const list<Ressources>& coutRessourcesMerveille = merveille.getCoutRessource();
+    vector<Ressources> ressourcesJoueur = getAllRessources();
+    // Parcourir les ressources requises pour construire la merveille
+    for (const Ressources& ressource : coutRessourcesMerveille) {
+        // Check si le joueur possede la ressource nécessaire
+        auto it = find(ressourcesJoueur.begin(), ressourcesJoueur.end(), ressource);
+        if (it != ressourcesJoueur.end()) {
+            // La ressource est trouvée, on la supprime du vecteur des ressources du joueur (pour prendre en compte les doublons)
+            ressourcesJoueur.erase(it);
+        } else {
+            // La ressource n'est pas trouvée
+            cout << "Vous n'avez pas assez de ressources pour construire ce bâtiment" << endl;
+            return;
+        }
+    }
+    ajouterMerveille(merveille);
+
+    if(merveille.getReplay()) effetRejouer = true;
+    pointsDeVictoire += merveille.getPointVictoire();
+    pieces += merveille.getRewardArgent();
+
+    // TODO : Ajout des effets de la merveille / ce qu'elle rapporte (militaire, ressouces, jetons, effets
+
 }
