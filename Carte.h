e6a683bb22e9c47044c7616ed2b391dcc050ee59
Nils <69086570+jin-sei@users.noreply.github.com>
2024-06-02 18:48:25 +0200

    Update Carte.h - écrasement header

--- Carte.h
+++ Carte.h
@@ -3,24 +3,33 @@
 #include <iostream>
 #include <string>
 #include <list>
+#include <vector>
+#include <map>
 #include <set>
+#include <algorithm>
+#include <random>
 
-#include "Type.h"
 
-#ifndef CARTE_H
-#define CARTE_H
-//enum class type_batiment { Militaire, Scientifique, Manufacture, Premiere, Civil, Commerce, Guilde, Merveille };
-//enum class JetonsProgres { Agriculture, Architecture, Economie, Loi, Maconnerie, Mathematiques, Philosophie, Strategie, Theologie, Urbanisme };
-//enum class phase_jeu { START, AGE_I, AGE_II, AGE_III, END };
-//enum class ressource { Bouclier, Papyrus, Verre, Pierre, Argile, Bois, Roue, Compas, Pilon, Tablette, Lyre, Mesure, Telescope };
+std::set<ressource> getMatierePremiere();
+std::set<ressource> getProduitManufacture();
+std::set<ressource> getRessource();
+std::set<ressource> getSymboleScientifique();
 
-std::set<Ressources> getMatierePremiere();
-std::set<Ressources> getProduitManufacture();
-std::set<Ressources> getRessource();
-std::set<Ressources> getSymboleScientifique();
+std::string tostringRessources(ressource r);
+std::string tostringType(type_batiment t);
+std::string tostringAge(phase_jeu p);
 
-std::string tostringRessources(Ressources r);
-std::string tostringType(Types t);
+phase_jeu& operator++(phase_jeu& phase);
+
+void displayRessources(std::list<ressource> r);
+
+class GameException {
+	public:
+		GameException(const std::string& i) :info(i) {}
+		std::string getInfo() const { return info; }
+	private:
+		std::string info;
+};
 
 // FORWARD DECLARATION
 class Perk ;
@@ -34,43 +43,43 @@ class Carte { // ABSTRACT
 
     public: 
 
-        Carte(std::string nom, Types type, phase_jeu age=phase_jeu::AGE_I, std::list<Ressources> cost_r = {}, unsigned int cost_m = 0, unsigned int argent = 0, unsigned int pt_victoire = 0);
+        Carte(std::string nom, type_batiment type, phase_jeu age=phase_jeu::AGE_I, std::list<ressource> cost_r = {}, unsigned int cost_m = 0, unsigned int argent = 0, unsigned int pt_victoire = 0);
 
         // GETTERS
         std::string getNom() const { return nom ; }
-        Types getType() const { return type ;}
+        type_batiment getType() const { return type ;}
         phase_jeu getAge() const { return c_age ; }
         unsigned int getCoutArgent() const { return cost_m ; }
         unsigned int getRewardArgent() const { return argent ; }
         unsigned int getPointVictoire() const { return pt_victoire ;}
-        std::list<Ressources> getCoutRessources() const { return cost_r ;}
+        std::list<ressource> getCoutRessource() const { return cost_r ;}
 
         // SETTERS
         void setNom(std::string new_nom){ nom = new_nom ;}
-        //void setType(Types new_type) {type = new_type ;} //  attribut const
+        //void setType(type_batiment new_type) {type = new_type ;} //  attribut const
         void setAge(phase_jeu p){ c_age = p ;}
         void setCoutArgent(unsigned int new_cost_m) { cost_m = new_cost_m ;}
         void setRewardArgent(unsigned int new_argent) { argent = new_argent ;}
         void setPointVictoire(unsigned int new_pt_victoire) { pt_victoire = new_pt_victoire ;}
-        void setCoutRessources(std::list<Ressources> cost) ;
+        void setCoutRessource(std::list<ressource> cost) ;
 
         // UTILS
-        std::list<Ressources> achetableRessources(std::list<Ressources> buy) const ; 
-        // retourne la liste des Ressources manquante pour acheter la Carte
+        std::list<ressource> achetableRessource(std::list<ressource> buy) const ; 
+        // retourne la liste des ressources manquante pour acheter la Carte
 
         virtual void affichage() const ;
         virtual void onBuild(Joueur* j) const = 0; // Joueur qui a construit la carte
     
     protected : 
 
-        bool checkTypeRessources(std::list<Ressources> cost_r, std::set<Ressources> subset) const;
-        // vérifier que les Ressources entrées appartiennent à la bonne catégorie
+        bool checkTypeRessources(std::list<ressource> cost_r, std::set<ressource> subset) const;
+        // vérifier que les ressources entrées appartiennent à la bonne catégorie
 
     private:
         std::string nom ;
-        const Types type ;
+        const type_batiment type ;
         phase_jeu c_age ; 
-        std::list<Ressources> cost_r ; // cout en Ressources
+        std::list<ressource> cost_r ; // cout en ressources
         unsigned int cost_m ; // cout en argent
 
         unsigned int argent ;
@@ -92,27 +101,27 @@ class Batiment : public Carte {
     public:
 
         Batiment(
-            std::string nom, Types type, phase_jeu age=phase_jeu::AGE_I,
-            std::list<Ressources> cost_r={}, unsigned int cost=0,
+            std::string nom, type_batiment type, phase_jeu age=phase_jeu::AGE_I,
+            std::list<ressource> cost_r={}, unsigned int cost=0,
             unsigned int argent=0, unsigned int pt_victoire=0,
 
-            std::list<Ressources> prod={}, std::string chained_by=""
+            std::list<ressource> prod={}, std::string chained_by=""
             );
         
         // GETTERS
         std::string getChainage() const { return chained_by;}
-        std::list<Ressources> getProduction() const { return production ;}
+        std::list<ressource> getProduction() const { return production ;}
 
         // SETTERS
         void setChainage(std::string new_chain) { chained_by = new_chain;}
-        void setProduction(std::list<Ressources> new_prod) { production = new_prod ;}
+        void setProduction(std::list<ressource> new_prod) { production = new_prod ;}
 
         void affichage() const override ;
         void onBuild(Joueur* j) const override ; 
 
 
     private:
-        std::list<Ressources> production ;
+        std::list<ressource> production ;
         std::string chained_by ; 
 };
 
@@ -120,7 +129,7 @@ class Commerce : public Batiment {
     // RESPONSABLE DE SA PERK !!!
     public:
 
-        Commerce(std::string nom, Types type, phase_jeu age=phase_jeu::AGE_I, std::list<Ressources> cost_r={}, unsigned int cost=0, unsigned int argent=0, unsigned int pt_victoire=0, std::list<Ressources> prod={}, std::string chained_by="", const Perk* perk=nullptr);
+        Commerce(std::string nom, type_batiment type, phase_jeu age=phase_jeu::AGE_I, std::list<ressource> cost_r={}, unsigned int cost=0, unsigned int argent=0, unsigned int pt_victoire=0, std::list<ressource> prod={}, std::string chained_by="", const Perk* perk=nullptr);
         ~Commerce();
         
         const Perk* getPerk() const { return perk; }
@@ -134,9 +143,9 @@ class Commerce : public Batiment {
 
 class Merveille : public Commerce {
     public:
-        Merveille(std::string nom, Types type=Types::Merveille, phase_jeu age=phase_jeu::AGE_I, std::list<Ressources> cost_r = {}, unsigned int cost_m = 0, unsigned int argent = 0, unsigned int pt_victoire = 0, std::list<Ressources> prod={}, std::string chained_by="", const Perk* perk=nullptr, bool b=false, Carte* f=nullptr) : 
+        Merveille(std::string nom, type_batiment type=type_batiment::Merveille, phase_jeu age=phase_jeu::AGE_I, std::list<ressource> cost_r = {}, unsigned int cost_m = 0, unsigned int argent = 0, unsigned int pt_victoire = 0, std::list<ressource> prod={}, std::string chained_by="", const Perk* perk=nullptr, bool b=false, Carte* f=nullptr) : 
         Commerce(nom, type, age, cost_r, cost_m, argent, pt_victoire, prod, chained_by, perk), replay(b), feed(f) {
-            if(type!=Types::Merveille){
+            if(type!=type_batiment::Merveille){
                 throw GameException("ERREUR: Merveille instanciée avec un type autre que Merveille");
             }
             if(chained_by != ""){
@@ -163,11 +172,11 @@ class Merveille : public Commerce {
 class Guilde : public Carte {
     public:
         Guilde(
-            std::string nom, Types type, phase_jeu age=phase_jeu::AGE_I,
-            std::list<Ressources> cost_r={}, unsigned int cost=0,
+            std::string nom, type_batiment type, phase_jeu age=phase_jeu::AGE_I,
+            std::list<ressource> cost_r={}, unsigned int cost=0,
             unsigned int argent=0, unsigned int pt_victoire=0,
 
-            std::list<Types> affectation={}, bool usurier=false
+            std::list<type_batiment> affectation={}, bool usurier=false
             );
 
         unsigned int ptVictoireFinJeu(Joueur* j) const ;
@@ -176,9 +185,10 @@ class Guilde : public Carte {
         void onBuild(Joueur* j) const override ;
 
     private:
-        std::list<Types> affectation ; 
+        std::list<type_batiment> affectation ; 
         bool usurier ; 
 };
+
 
 class Perk { // ABSTRACT
 
@@ -197,7 +207,7 @@ class Perk { // ABSTRACT
 class Perk_CoinPerCard : public Perk {
 
     public: 
-        Perk_CoinPerCard(unsigned int coin, Types card):coin(coin), card(card){}
+        Perk_CoinPerCard(unsigned int coin, type_batiment card):coin(coin), card(card){}
         //~Perk_CoinPerCard(){};
 
         void gainCoinPerCard(Joueur* j) const ;
@@ -205,29 +215,29 @@ class Perk_CoinPerCard : public Perk {
 
     private: // perk settings
         unsigned int coin ; // nombre de pièces que l'on gagne par carte
-        Types card ; // type de carte sur laquelle le calcul se base
+        type_batiment card ; // type de carte sur laquelle le calcul se base
 
 }; 
 
 class Perk_Destruction : public Perk { // requires player interaction
     public:
-        Perk_Destruction(Types c);
+        Perk_Destruction(type_batiment c);
 
         void destruction(Joueur* j) const ; 
         void onCall(Joueur* j) const override; 
     private:
-        Types card ; // type de Bâtiment autorisé à la destruction
+        type_batiment card ; // type de Bâtiment autorisé à la destruction
 };
 
 class Perk_FixedTrade : public Perk {
 
     public:
-        Perk_FixedTrade(std::list<Ressources> res, unsigned int coin): res(res), coin(coin){}
+        Perk_FixedTrade(std::list<ressource> res, unsigned int coin): res(res), coin(coin){}
 
         void setFixedTrade(Joueur* j) const ;
         void onCall(Joueur* j) const override ; 
     private: // perk settings
-        std::list<Ressources> res ; // Ressources pour laquelle le prix de trade est fixé
+        std::list<ressource> res ; // ressource pour laquelle le prix de trade est fixé
         unsigned int coin ; // prix fixé
 
 };
@@ -237,7 +247,7 @@ class Perk_Classic : public Perk { // PERKS W/O SETTINGS
 // FREE CONSTRUCTION FROM DEFAUSSE
 // SACCAGE
     public: 
-        Perk_Classic(unsigned int id, const GameManager* box);
+        Perk_Classic(unsigned int id, const Box* box);
 
         void saccage(Joueur* j) const ;
         void freeConstructionFromDefausse(Joueur* j) const;
@@ -245,5 +255,22 @@ class Perk_Classic : public Perk { // PERKS W/O SETTINGS
         void onCall(Joueur* j) const override;
     private:
         unsigned int id ;
-        const GameManager* box ; 
+        const Box* box ; 
 };
+
+/* 
+
+// La Perk PolyRessource est abandonnée
+// On considère à la place que si un bâtiment de Commerce ou une Merveille produit des ressources,
+// il s'agit d'une poly ressource
+
+class Perk_PolyRessource : public Perk {
+    public:
+        Perk_PolyRessource(std::list<ressource> res):res(res){}
+
+        std::list<ressource> getPolyRessources() const ;
+        void onCall(Joueur* j) const override ; 
+    private:
+        std::list<ressource> res;
+};
+*/
