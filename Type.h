7fa2f80e45ccd642968e3dc1b41b0621338afb11
Nils <69086570+jin-sei@users.noreply.github.com>
2024-06-02 18:47:03 +0200

    Update Type.h - écrasement

--- Type.h
+++ Type.h
@@ -1,31 +1,5 @@
-enum class Ressources {Bouclier=0, Papyrus=1, Verre=2, Pierre=3, Argile=4, Bois=5, Telescope=6, Roue=7, Cadran=8, Pilon=9, Compas=10, Plume=11, Balance=12};
 
-enum class JetonsProgres{
-    Agriculture, Architecture, Economie, Loi, Maconnerie, Mathematiques,
-    Philosophie, Strategie, Theologie, Urbanisme
-};
-
-/*
-enum class SymboleScientifiques{
-    Roue, Compas, Mortier_Pilon, Tablette, Lyre, Mesure, Telescope
-};
-
-
-enum class produitManufacture{
-    Verre, Papyrus
-};
-*/
-
-enum class Types{
-    Scientifique, Commerce, Militaire, Civil, Premiere, Manufacture, Merveille, Guilde
-};
-//enum class Couleurs{ marron, gris, bleu, vert, jaune, rouge};
-
-/*
-enum class Chainage{
-    Livre, Engrenage, Masque, Lune, Eau, Sabot, Epee, Tour, Cible, Chevalier, Lyre, Theiere,
-    Soleil, Colonne, Tambour, Vase, Batiment
-};
-*/
-
-enum class phase_jeu { START, AGE_I, AGE_II, AGE_III, END };
+enum class type_batiment {Militaire, Scientifique, Manufacture, Premiere, Civil, Commerce, Guilde, Merveille};
+enum class jeton_progres{Agriculture, Architecture, Economie, Loi, Maconnerie, Mathematiques,Philosophie, Strategie, Theologie, Urbanisme};
+enum class phase_jeu {START=0, AGE_I=1, AGE_II=2, AGE_III=3, END=4};
+enum class ressource {Bouclier=0, Papyrus=1, Verre=2, Pierre=3, Argile=4, Bois=5, Telescope=6, Roue=7, Cadran=8, Pilon=9, Compas=10, Plume=11, Balance=12};
